792a9921c844268085e0ad301bac596f13920233
noble-47 <noblebest56@gmail.com>
2022-07-31 14:29:42 +0100

    updated pipfile

--- backend/price_compare/settings.py
+++ backend/price_compare/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from dotenv import load_env
+from dotenv import load_dotenv
 from pathlib import Path
 
 import os
