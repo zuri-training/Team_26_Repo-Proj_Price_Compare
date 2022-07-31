15f51b66999ae8d223ca7d6894e3ef7037457640
alaswadiyy <abdulmuizsharafadeen@gmail.com>
2022-07-31 14:32:11 +0100

    add text content to about, compare, cookie and privacy pages

--- frontend/price_beta/src/pages/ComparePricePage.js
+++ frontend/price_beta/src/pages/ComparePricePage.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 const ComparePricePage = () => {
   return (
-    <div>ComparePricePage</div>
+    <div>
+      <h1>How we compare prices</h1>
+      <p>We hope is to be a complete and unbiased price and product comparison service. We list all the shops and prices that we know of for any single product. We list both the shops that pay us for the traffic we send them (featured shops) as well as the shops that do not. We do not exclude any offers.</p>
+      <ol>
+          <li>How our prices are shown</li>
+          <p>The is no beneficial ranking of shops which sell specific or the same products. For products sold in various shops, The list of features are displayed by:</p>
+          <ul>
+              <li>Prices</li>
+              <li>Shipping status</li>
+              <li>Stock status</li>
+              <li>Shop ratings</li>
+              <li>Number of shops</li>
+              <li>Number of Products the shop sells at low price</li>
+          </ul>
+          <p>The prices we show are excluding shipping, unless we specify otherwise. Keep in mind that shops may charge fees that we do not show, such as service fees, environmental fees, and deposit return fees.</p>
+          <li>How our shops are shown</li>
+          <p>A featured shop is displayed with its own logotype in our price lists. They also have a short description written by the shop itself. Featured shops have a green button that directs the visitor to the shop, or a link that differs from those of the non-featured shops.</p>
+          <p>Non-featured shops have no logotype in our price lists. They also have no description. Non-featured shops may have less distinctive links leading to the shop, or have no links at all</p>
+          <p>There are no other differences in how offers from featured and non-featured shops are displayed.</p>
+          <p>In places where ScoutVendor displays a single price for a product, with additional information about the shop, a featured shop is shown with a logotype while a non-featured shop has its name in text only.</p>
+          <p>A featured shop either pays us a fixed fee for each click we redirect to them or a small percentage of the order value (if an order was placed after being redirected from ScoutVendor). ScoutVendor has either direct contracts with the shops or utilises existing affiliate networks.</p>
+          <p>No fees or additional costs are added to the products listed at ScoutVendor. The prices listed at ScoutVendor should never be higher than if visiting the web shop directly without being redirected from our service.</p>
+          <li>Shop listed on ScoutVendor</li>
+          <p>Our aim is to list all national shops that are offering products in any of the categories currently listed at ScoutVendor.</p>
+          <p>We do our best to find all shops online ourselves, but if you own or know of a shop missing from our service, selling products in categories listed at ScoutVendor. Contact Us for more information.</p>
+          <li>Price Updates and Variants</li>
+          <p>Our product-related price information, including price and stock status, is usually updated three to five times a day. Under certain circumstances, information may be updated more often or more rarely. We receive product-related information from the shops themselves or collect it by automatic web scraping. Our aim is to always show every single product a shop lists in every product category that we have, but in rare cases, we will not be able to. The shop could have a structure too difficult to handle or they could actively be trying to block us. In these cases, we show as much information as we can while trying to work with the shop to find a better solution.</p>
+          <p>Despite ScoutVendor’s efforts to always show correct information about all products, the information (this applies not only to price, but pictures, product properties, availability and delivery time too) comes from the shops themselves. Therefore, even if we do our very best, errors may occur. Always check the shop’s own price and description of the product before making a final shopping decision.</p>
+          <p>The same shop may offer a product in many different variations. It can be anything from a variety of colours, language versions or packaged versions where the product comes with different accessories. In order to avoid information-heavy price lists, such list items can be expanded to show all variations of the product. All product variants from a single shop will be sorted according to price, from lowest to highest.</p>
+      </ol>
+    </div>
   )
 }
 
--- frontend/price_beta/src/pages/PrivacyPolicyPage.js
+++ frontend/price_beta/src/pages/PrivacyPolicyPage.js
@@ -2,7 +2,46 @@ import React from 'react'
 
 const PrivacyPolicyPage = () => {
   return (
-    <div>PrivacyPolicyPage</div>
+    <div>
+      <h1>Privacy Policy</h1>
+      <ol>
+        <li>Information</li>
+        
+        <p>Sc outVendor priortises your privacy. To protect your privacy we provide this notice explaning our online information practices and the choices you can make with regard to the way your information is collected and used.</p>
+        <p>The terms ScoutVendor, the website, us and we refer to the owner of the website whose registration office is ScoutVendorNet SRL. The term “you” refers to the viewer of our site.</p>
+        <p>By  accessing the website ScoutVendor, you accept and agree to be bound by the Privacy Policy. If you do not accept the terms contained in this Privacy Policy statement, you must immediately cease using the website and/or services.</p>
+        <p>ScoutVendor may change this policy from time to time by updating this page. You should check this page periodically to ensure you are aware of all the changes.</p>
+        <p>ScoutVendor Net SRL nad ScoutVendor are commited to ensuring that your privacy is protected. If we ask you to provide certain information by which you can be identified when using this website, we garantee that it will be used in accordance to this Privacy statement.</p>
+        <p>Our site is not targeted for use by anyone under the age of 18. By using this website, you warrant that you are at least 18 years old. We reserve the right to terminate your membership if any information may lead us to discover the fact that you are less that 18.</p>
+        <p>Persons under 18 may not submit any personally identifiable information on the Website and if it is discoverd that such information has been inadvertently gathered, we reserve the right to delete it.</p>
+        <p>PriceBeta uses the information voluntarily submitted by users in order to enhance their experience on its network of sites, to provide interactice or personalised elements or to deliver preference-based content to the users.</p>
+        
+        <li>Cookies and Gathering User Technical Data</li>
+        <p>This information is required in order for us to understand your needs, to provide you with better service and also for the following reasons.</p>
+        <ul>
+            <li>Improved customer service</li>
+            <li>Improved website features</li>
+            <li>Personalise user experience</li>
+            <li>Internal record keeping</li>
+            <li>Improve our product and services</li>
+        </ul>
+        <p>The ways in which information may be gathered include the following:</p>
+        <ul>
+            <li>When you visit ScoutVendor, we collect your IP address, such as your browser type and the pages you have accessed on our website. This information will be collected and saved our Net SRL.</li>
+            <li>To personalise and enhance your browsing and shopping experience, we may place in a text file called a “cookie” in the browser files of your computer. Although the cookie itself does not contain Personal Information, it will allow us to relate your use of this site to the information you have specifically and knowingly provided.</li>
+        </ul>
+        <p>The only personal information a cookie can contain is the information that you provide yourself. A cookie cannot read data from your hard disk, cannot read cookie files created by other sites and only the web site that transferred a particular cookie to you can read, modify or delete such cookie. PriceBeta uses cookies to track user traffic patterns (as described above). Our advertising system delivers a one-time cookie to better track ad impressions and click rates. You can refuse cookies by turning them off in your browser.</p>
+        <p>By disabling the cookies in your browser, please be aware that you may not be able to use the Website properly and that you may not be able to access some important sections and pages of the Website, such as product pages, searches, comparison pages. If you have set your browser to warn you before accepting cookies, you will receive a warning message with each cookie.</p>
+        <p>At the present time it is not technically possible for us to allow you to carry your cookie settings with you between your browsers and devices so you will need to change these settings from each browser you use.</p>
+        <p>Please also be aware that we make every effort to respect your choices, however, there is the possibility that not all cookies will be captured. If this is a concern then we would recommend that you change your cookie settings via your browser; your browser help function will tell you how.</p>
+        <li>Personal Data Access</li>
+        <p>We do not sell, trade, or otherwise transfer your personally identifiable information to third parties. This does not include trusted third parties who assist us in operating our website, conducting our business, or providing you with services, as long as those parties are committed to keeping this information confidential by all means. We may also release your information when such a release is necessary in order to comply with the law, enforce our site policies or protect the rights, property or safety of ScoutVendor Net SRL or of other parties. However, non-personally identifiable visitor information may be provided to other parties for marketing and advertising purposes or for other uses.</p>
+        <li>Data Security</li>
+        <p>We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we use advanced physical, electronic and managerial procedures to safeguard and secure the information that we collect online.</p>
+        <li>External Links</li>
+        <p>The site may contain links, ads or advertising banners that will lead you to other sites, belonging to third parties. Any kind of information, including personal information, given to such pages is not under ScoutVendor Net SRL’s jurisdiction or authority, and this company cannot be held responsible for any usage of these sites. If you have concerns about how other web sites collect and use your personal information, make sure to read that site's own privacy policy. ScoutVendor is not responsible for the privacy practices or for the content of these sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites</p>
+      </ol>
+    </div>
   )
 }
 
