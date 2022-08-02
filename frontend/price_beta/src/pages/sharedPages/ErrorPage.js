9fe91e7c13410e3e2c2ff9c7d75faf1efbf0a346
Chibuzor Arthur Uzoaru <chibuzorarthur@gmail.com>
2022-08-02 23:32:50 +0100

    Add error page

--- frontend/price_beta/src/pages/sharedPages/ErrorPage.js
+++ frontend/price_beta/src/pages/sharedPages/ErrorPage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import error_img from "../../assets/images/error-img.svg";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div>
+      <main className="error-container">
+        <article className="error_text_container">
+          {/* Error message */}
+          <h1 className="error_h1">Ooops...</h1>
+          <h1 className="error_h1">Page not found</h1>
+          <p className="error_paragraph">
+            The page you are looking for doesn’t exist or an error occured, go
+            back to home page
+          </p>
 
-export default ErrorPage
+          {/* Go Back Button */}
+          <button className="go_bck_btn" id="btn">
+            Go Back
+          </button>
+        </article>
+        <div className="error_img_div">
+          <figcaption>
+            <img className="error_img" src={error_img} alt="page not found" />
+          </figcaption>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default ErrorPage;
