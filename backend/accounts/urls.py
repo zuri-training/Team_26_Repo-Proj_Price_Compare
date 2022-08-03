e372d3c2e35891264f199d4573b59c21def09f0b
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-08-03 18:11:30 +0100

    added and modified new paths

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import EmailVerifyView,LoginView, LogoutView, RegisterView, UserView
+from .views import RegisterView, EmailVerifyView,LoginView, UserView,RequestPasswordResetEmail, PasswordResetTokenCheckView, SetNewPasswordView, LogoutView
 
 app_name = 'accounts'
 
 urlpatterns =[
-    path('register', RegisterView.as_view()),
+    path('register', RegisterView.as_view(), name='register'),
     path('verify-email', EmailVerifyView.as_view(), name='email-activate'),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view())
+    path('login', LoginView.as_view(), name = 'login'),
+    path('user', UserView.as_view(), name = 'user'),
+    path('request-password-reset', RequestPasswordResetEmail.as_view(), name = 'request-password-reset'),
+    path('reset-password/<uidb64>/<token>', PasswordResetTokenCheckView.as_view(), name = 'reset-password'),
+    path('reset-password-completed', SetNewPasswordView.as_view(), name = 'reset-password-completed'),
+    path('logout', LogoutView.as_view(), name = 'logout')
 ]
 
