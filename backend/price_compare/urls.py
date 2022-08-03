391f803c87e5330c62faab017a5ba0774cd76e71
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-08-03 18:15:55 +0100

    authentication

--- backend/price_compare/urls.py
+++ backend/price_compare/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('acounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('api/', include('accounts.urls')),
+    path('api/auth/', include('accounts.urls')),
 ]
