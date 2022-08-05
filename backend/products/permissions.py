8adbb283a36bec3149d11e07acc4787e71710907
Mally13 <mallymutuku13@gmail.com>
2022-08-06 00:25:51 +0300

    small changes

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -1,12 +1,12 @@
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 
-from rest_framework.permissions import BasePermission
+# from rest_framework.permissions import BasePermission
 
-Scrapper = User.objects.get(email="scrapper@email.com")
+# Scrapper = User.objects.get(email="scrapper@email.com")
 
 
-class AllowScrapper(BasePermission):
-    "Allows access to only our Scrapper"
+# class AllowScrapper(BasePermission):
+#     "Allows access to only our Scrapper"
 
-    def has_permission(self, request, view):
-        return True if request.user == Scrapper else False
+#     def has_permission(self, request, view):
+#         return True if request.user == Scrapper else False
