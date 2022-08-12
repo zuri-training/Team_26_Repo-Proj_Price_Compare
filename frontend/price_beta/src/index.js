e96fdb30e608b14818d93e15a542ac76bcf1fdf5
Bridget <bridgetlawani09@gmail.com>
2022-08-12 23:57:55 +0100

    modified pages

--- frontend/price_beta/src/index.js
+++ frontend/price_beta/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import App from './App';
-import { store } from './store';
-import { Provider } from 'react-redux'
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <ProductsProvider>
+        <FilterProvider>
+          <App />
+        </FilterProvider>
+      </ProductsProvider>
   </React.StrictMode>
 );
-
